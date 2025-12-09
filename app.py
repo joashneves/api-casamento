b4811a837656f65fba1866547eb6d47129643602
joashneves <joashneves@gmail.com>
2025-12-09 15:22:06 -0300

    libera metodos

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 from models.rsvp import db, RSVP
 db.init_app(app)
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
 @app.route('/')
 def hello_world():
     return jsonify({"message": "Olá, mundo!"})
